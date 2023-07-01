5d212c3e3314cf6393c0eb7b765830765aae6658
Aashray Bhandari <aashraybhandari@Aashrays-MacBook-Air.local>
2023-07-01 00:25:08 -0600

    Code to load basedata: Team and Venue data

--- basedata-service/src/main/java/com/scoretracker/basedataservice/model/Stadium.java
+++ basedata-service/src/main/java/com/scoretracker/basedataservice/model/Stadium.java
@@ -1,5 +1,8 @@
 package com.scoretracker.basedataservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,16 +10,20 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(value="stadium")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Stadium {
     @Id
+    @JsonIgnore
     private String id;
+    @JsonProperty("id")
     private String footballApiId;
     private String name;
     private String city;
     private int capacity;
+    private String image;
 }
--- basedata-service/src/main/java/com/scoretracker/basedataservice/model/Team.java
+++ basedata-service/src/main/java/com/scoretracker/basedataservice/model/Team.java
@@ -1,5 +1,8 @@
 package com.scoretracker.basedataservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,14 +10,17 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(value="team")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Team {
     @Id
+    @JsonIgnore
     private String id;
+    @JsonProperty("id")
     private String footballApiId;
     private String name;
     private String code;
@@ -22,4 +28,5 @@ public class Team {
     private String homeStadiumId;
     private String country;
     private int founded;
+    private String logo;
 }
