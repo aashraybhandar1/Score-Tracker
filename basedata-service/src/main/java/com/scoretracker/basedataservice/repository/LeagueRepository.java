2d9cb418e517fadaae0df39d73d940a364184a7d
Aashray Bhandari <aashraybhandari@Aashrays-MacBook-Air.local>
2023-07-02 11:51:46 -0600

    Changes to query the mongo db correctly

--- basedata-service/src/main/java/com/scoretracker/basedataservice/repository/LeagueRepository.java
+++ basedata-service/src/main/java/com/scoretracker/basedataservice/repository/LeagueRepository.java
@@ -3,6 +3,7 @@ package com.scoretracker.basedataservice.repository;
 import com.scoretracker.basedataservice.model.League;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Set;
 
 public interface LeagueRepository extends MongoRepository<League,String> {
--- basedata-service/src/main/java/com/scoretracker/basedataservice/service/LeagueCodeService.java
+++ basedata-service/src/main/java/com/scoretracker/basedataservice/service/LeagueCodeService.java
@@ -3,10 +3,14 @@ package com.scoretracker.basedataservice.service;
 import com.mongodb.client.DistinctIterable;
 import com.mongodb.client.MongoCollection;
 import com.scoretracker.basedataservice.model.League;
+import com.scoretracker.basedataservice.repository.LeagueRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @RequiredArgsConstructor
@@ -14,9 +18,10 @@ import java.util.Set;
 public class LeagueCodeService {
     private final MongoTemplate mongoTemplate;
 
-    public Set<String> fetchLeagueCode(){
-        DistinctIterable<String> temp = mongoTemplate.getCollection("league").distinct("footballApiID", String.class);
-        String abc = "vhbj";
-        return null;
+    public List<String> fetchLeagueCode(){
+        Query query = new Query();
+        query.fields().include("footballApiID");
+        query.addCriteria(Criteria.where("footballApiID").exists(true));
+        return mongoTemplate.findDistinct(query,"footballApiID",League.class, String.class);
     }
 }
