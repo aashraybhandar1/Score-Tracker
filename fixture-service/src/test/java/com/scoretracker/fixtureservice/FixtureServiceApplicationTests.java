df26998474e3aa2ad4d59eb0b7745056af329b12
Aashray Bhandari <aashraybhandari@Aashrays-MacBook-Air.local>
2023-07-03 10:49:08 -0600

    Skeleton code for fixture service

---
+++ fixture-service/src/test/java/com/scoretracker/fixtureservice/FixtureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoretracker.fixtureservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixtureServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
