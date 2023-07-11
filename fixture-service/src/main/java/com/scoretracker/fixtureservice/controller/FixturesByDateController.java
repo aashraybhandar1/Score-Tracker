3dc0f1cfdac36b5a65c048053e6c7f4412e2e044
Aashray Bhandari <aashraybhandari@Aashrays-MacBook-Air.local>
2023-07-11 09:09:35 -0700

    temporary changes for fixture date service

--- fixture-service/src/main/java/com/scoretracker/fixtureservice/controller/FixturesByDateController.java
+++ fixture-service/src/main/java/com/scoretracker/fixtureservice/controller/FixturesByDateController.java
@@ -3,6 +3,7 @@ package com.scoretracker.fixtureservice.controller;
 import com.scoretracker.fixtureservice.service.FixtureByDateService;
 import com.scoretracker.fixtureservice.service.SeasonFixtureService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,8 @@ public class FixturesByDateController {
     private final FixtureByDateService fixtureByDateService;
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public String fetchFixturesByDate(@RequestParam Date date){
+    public String fetchFixturesByDate(@RequestParam @DateTimeFormat(pattern="MMddyyyy") Date date){
+        fixtureByDateService.fetchFixturesByDate(date);
         return "";
     }
 }
