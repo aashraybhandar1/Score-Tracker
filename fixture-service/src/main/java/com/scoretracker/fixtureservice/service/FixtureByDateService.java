d26439faf26ccbb4caf7138a1b67d305c581674a
Aashray Bhandari <aashraybhandari@Aashrays-MacBook-Air.local>
2023-07-12 01:12:46 -0700

    Fixture by date service ensure db connectivity and appropriate data mapping

--- fixture-service/src/main/java/com/scoretracker/fixtureservice/service/FixtureByDateService.java
+++ fixture-service/src/main/java/com/scoretracker/fixtureservice/service/FixtureByDateService.java
@@ -5,20 +5,25 @@ import com.scoretracker.fixtureservice.model.Fixture;
 import com.scoretracker.fixtureservice.repository.FixtureRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class FixtureByDateService {
     private final FixtureRepository fixtureRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
-    public String fetchFixturesByDate(Date date){
-        ArrayList<Fixture> result = (ArrayList<Fixture>) fixtureRepository.getFixtureDataByDate(date);
-        String temp = "";
-        return temp;
+    public List<FixtureByDate> fetchFixturesByDate(Date date){
+        return modelMapper.map(fixtureRepository.getFixtureDataByDate(date), new TypeToken<List<FixtureByDate>>() {}.getType());
+
     }
 }
