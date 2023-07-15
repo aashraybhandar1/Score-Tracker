0ada6346a209b74df45eaf818ebfd6fb97461b09
Aashray Bhandari <aashraybhandari@Aashrays-MacBook-Air.local>
2023-07-15 16:31:09 -0700

    Implemented cron job to fetch fixture updates in the next 7 days

--- fixture-service/src/main/java/com/scoretracker/fixtureservice/repository/FixtureRepository.java
+++ fixture-service/src/main/java/com/scoretracker/fixtureservice/repository/FixtureRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface FixtureRepository extends JpaRepository<Fixture, Long> {
     @Query(value = "Select * From t_fixture Where Date(date) = Date(:date)",
             nativeQuery = true)
@@ -18,6 +20,12 @@ public interface FixtureRepository extends JpaRepository<Fixture, Long> {
 
     @Transactional
     @Modifying
-    @Query("UPDATE t_fixture f SET f.timestamp = :timestamp AND f.DATE = :date WHERE f.footballApiId = :footballApiId")
-    void updateTimestampByFootballApiId(@Param("timestamp") String timestamp, @Param("date") Date date, @Param("footballApiId") String footballApiId);
+    @Query(value="Select f.footballApiId, f FROM Fixture f WHERE f.footballApiId in :footballApiId")
+    List<Object[]> getFixturesFromFixtureId(@Param("footballApiId") List<String> footballApiId);
+
+    @Transactional
+    @Modifying
+    @Query(value="UPDATE t_fixture f SET f.timestamp = :timestamp WHERE f.football_api_id = :footballApiId",
+            nativeQuery = true)
+    void updateTimestampByFootballApiId(@Param("timestamp") String timestamp, @Param("footballApiId") String footballApiId);
 }
--- fixture-service/src/main/java/com/scoretracker/fixtureservice/service/FixtureUpdateService.java
+++ fixture-service/src/main/java/com/scoretracker/fixtureservice/service/FixtureUpdateService.java
@@ -4,22 +4,24 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scoretracker.fixtureservice.model.Fixture;
 import com.scoretracker.fixtureservice.repository.FixtureRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
@@ -38,49 +40,82 @@ public class FixtureUpdateService {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Scheduled(fixedDelay = 120000)
-    public void getUpdatedFixtures() {
-        LocalDate tomorrow = LocalDate.now().plusDays(1);
+    /*
+        Cron job to fetch fixtures for the next 7 days. Looks to update the fixture database in case of some
+        disruption.
 
+        Does bulk updates for the fetched results.
+     */
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void getUpdatedFixturesNextWeek() {
         List<String> leagueCodes = Arrays.asList(webClientBuilder.build().get()
                 .uri("http://localhost:8080/api/fetchLeagueCodes")
                 .retrieve()
                 .bodyToMono(String[].class)
                 .block());
-        String response = webClientBuilder.exchangeStrategies(ExchangeStrategies.builder()
-                        .codecs(configurer -> configurer
-                                .defaultCodecs()
-                                .maxInMemorySize(16 * 1024 * 1024))
-                        .build())
-                .build().get().uri(rapidApiFixtureUrl + "date=" + tomorrow.toString())
-                .header("X-RapidAPI-Key", rapidApiKey)
-                .header("X-RapidAPI-Host", rapidApiHost)
-                .retrieve()
-                .bodyToMono(String.class).block();
 
-        try {
-            JsonNode jsonNode = objectMapper.readTree(response);
-            JsonNode fixturesResponse = jsonNode.get("response");
+        Map<String, Fixture> resultMap = new HashMap<String, Fixture>();
 
-            AtomicInteger count  = new AtomicInteger();
-            fixturesResponse.forEach(fixtureResponse -> {
-                if(!leagueCodes.contains(fixtureResponse.get("league").get("id"))){
-                    count.getAndIncrement();
-                    String updatedTimeStamp = String.valueOf(fixtureResponse.get("fixture").get("timestamp"));
-                    String footballApiId = String.valueOf(fixtureResponse.get("fixture").get("id"));
-                    Date updatedDate = new Date(String.valueOf((fixtureResponse.get("fixture").get("date"))));
-                    String bleh = "ho";
-                    fixtureRepository.updateTimestampByFootballApiId(updatedTimeStamp, updatedDate, footballApiId);
-                    if(count.get()%5==0){
-                        entityManager.flush();
-                        entityManager.clear();
+        IntStream.range(1,8).forEach(daysAhead -> {
+            LocalDate day = LocalDate.now().plusDays(daysAhead);
+            String response = webClientBuilder.exchangeStrategies(ExchangeStrategies.builder()
+                            .codecs(configurer -> configurer
+                                    .defaultCodecs()
+                                    .maxInMemorySize(16 * 1024 * 1024))
+                            .build())
+                    .build().get().uri(rapidApiFixtureUrl + "date=" + day.toString())
+                    .header("X-RapidAPI-Key", rapidApiKey)
+                    .header("X-RapidAPI-Host", rapidApiHost)
+                    .retrieve()
+                    .bodyToMono(String.class).block();
+
+            try {
+                JsonNode jsonNode = objectMapper.readTree(response);
+                JsonNode fixturesResponse = jsonNode.get("response");
+
+                List<String> updatedTimeStamps = new ArrayList<String>();
+                List<String> footballApiIds = new ArrayList<String>();
+                List<Date> updatedDates = new ArrayList<Date>();
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.ENGLISH);
+                fixturesResponse.forEach(fixtureResponse -> {
+                    if(leagueCodes.contains(fixtureResponse.get("league").get("id"))){
+                        try {
+                            updatedTimeStamps.add(String.valueOf(fixtureResponse.get("fixture").get("timestamp")));
+                            footballApiIds.add(String.valueOf(fixtureResponse.get("fixture").get("id")));
+
+                            String date = String.valueOf((fixtureResponse.get("fixture").get("date")));
+                            updatedDates.add(formatter.parse((date.substring(1,date.length()-1))));
+                        }
+                        catch (ParseException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
-                }
-            });
-        } catch (JsonMappingException e) {
-            throw new RuntimeException(e);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+                });
+                List<Object[]> results = fixtureRepository.getFixturesFromFixtureId(footballApiIds);
+                Map<String, Fixture> resultMapAdd = results.stream()
+                        .collect(Collectors.toMap(
+                                result -> (String) result[0],
+                                result -> (Fixture) result[1]
+                        ));
+                AtomicInteger counter = new AtomicInteger();
+                footballApiIds.forEach(footballApiId -> {
+                    Fixture fixture = resultMapAdd.getOrDefault(footballApiId, null);
+                    if(fixture != null){
+                        fixture.setDate(updatedDates.get(counter.get()));
+                        fixture.setTimestamp(updatedTimeStamps.get(counter.get()));
+                        resultMapAdd.put(footballApiId, fixture);
+                    }
+                    counter.getAndIncrement();
+                });
+                resultMap.putAll(resultMapAdd);
+
+            } catch (JsonMappingException e) {
+                throw new RuntimeException(e);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        fixtureRepository.saveAll(resultMap.values());
     }
 }
