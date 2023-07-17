a9aa40556128f43916c39f1dbac0d42e92804005
Aashray Bhandari <aashraybhandari@Aashrays-MacBook-Air.local>
2023-07-17 12:06:07 -0700

    Signup service implemented

---
+++ authentication-service/controllers/usersController.go
@@ -0,0 +1,50 @@
+package controllers
+
+import (
+	"authentication-service/initializers"
+	"authentication-service/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Signup(c *gin.Context) {
+	//Get email/pass from req.body
+
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
+	}
+	//Hash the password
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to has password",
+		})
+		return
+	}
+
+	//Create the User
+
+	user := models.User{Email: body.Email, Password: string(hash)}
+	result := initializers.DB.Create(&user)
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create User",
+		})
+		return
+	}
+
+	//Respond
+	c.JSON(http.StatusOK, gin.H{})
+}
--- authentication-service/main.go
+++ authentication-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"authentication-service/controllers"
 	"authentication-service/initializers"
 
 	"github.com/gin-gonic/gin"
@@ -15,10 +16,6 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/signup", controllers.Signup)
 	r.Run()
 }
